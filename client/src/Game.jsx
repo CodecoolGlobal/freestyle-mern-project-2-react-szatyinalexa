ffc6a295301302bafa80e1239200eb96ff978219
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-02-01 21:28:09 +0100

    displaying score upon winning only when logged in user

--- client/src/Game.jsx
+++ client/src/Game.jsx
@@ -156,7 +156,7 @@ function Game() {
             <h1 className="winner-message">
               Congratulations, you matched all the kitties!
               {playPurr()}
-              <h3 className="current-score">Score: {user?.score}</h3>
+              {user && <h3 className="current-score">Score: {user?.score}</h3>}
             </h1>
           </div>
         ) : cats ? (

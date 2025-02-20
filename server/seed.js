d48b87a08f259acccf4e9b1314ea6d814af23e5b
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-02-20 12:38:52 +0100

    changing initial image count in seed.js

--- server/seed.js
+++ server/seed.js
@@ -2,6 +2,7 @@ import { config } from "dotenv";
 import mongoose from "mongoose";
 import MemoUser from "./model/User.js";
 import CatPic from "./model/CatPic.js";
+import { fetchValidImages } from "./fetchValidImages.js";
 config();
 
 async function seedUsers() {
@@ -48,37 +49,10 @@ async function seedCatPics() {
   }
 }
 
-async function fetchValidImages(requiredCount) {
-  const apiKey = process.env.CAT_API_KEY;
-  const apiUrl = `https://api.thecatapi.com/v1/images/search?limit=50&mime_types=jpg,png&api_key=${apiKey}`;
-  const validImageSet = new Set();
-  
-  while (validImageSet.size < requiredCount) {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-  
-    data.forEach((datum) => {
-      const idealWidthHeightRatio = 0.8;
-      const widthHeightRatio = datum.width / datum.height;
-        if (widthHeightRatio >= idealWidthHeightRatio - 0.2 && widthHeightRatio <= idealWidthHeightRatio + 0.2) {
-          const catPic = {
-            link: datum.url,
-            width: datum.width,
-            height: datum.height,
-            usedCount: 0,
-            createdAt: Date.now(),
-          }
-          validImageSet.add(catPic);
-        }
-    });
-  }
-  return Array.from(validImageSet).slice(0, requiredCount);
-}
-
 async function main() {
   await mongoose.connect(process.env.DATABASE_URL);
 
-  await seedUsers();
+  //await seedUsers();
 
   await seedCatPics();
 
